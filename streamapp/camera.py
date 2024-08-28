84aaac76828cd63c3f232f1f7f209beb6b71ef96
mtsfrancisco <matheusdabmlj@gmail.com>
2024-08-28 10:26:49 -0300

    excluindo codigo inutil

--- streamapp/camera.py
+++ streamapp/camera.py
@@ -3,11 +3,6 @@ import numpy as np
 from django.conf import settings
 from ultralytics import YOLO
 
-face_detection_videocam = cv2.CascadeClassifier(os.path.join(
-			settings.BASE_DIR,'opencv_haarcascade_data/haarcascade_frontalface_default.xml'))
-face_detection_webcam = cv2.CascadeClassifier(os.path.join(
-			settings.BASE_DIR,'opencv_haarcascade_data/haarcascade_frontalface_default.xml'))
-
 
 yolo = YOLO('yolo_model/best.pt')
 
@@ -25,7 +20,6 @@ class VideoCamera(object):
 
 	def __del__(self):
 		self.video.release()
-
 
 	def get_frame(self):
 		success, image = self.video.read()
@@ -52,8 +46,6 @@ class VideoCamera(object):
 					cv2.rectangle(image, (x1, y1), (x2, y2), colour, 2)
 					cv2.putText(image, f'{classes_names[int(box.cls[0])]} {box.conf[0]:.2f}', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, colour, 2)
 
-
-		#frame_flip = cv2.flip(image,1)
 		ret, jpeg = cv2.imencode('.jpg', image)
 		return jpeg.tobytes()
 
@@ -70,13 +62,6 @@ class IPWebCam(object):
 		imgResp = urllib.request.urlopen(self.url)
 		imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
 		img= cv2.imdecode(imgNp,-1)
-		# We are using Motion JPEG, but OpenCV defaults to capture raw images,
-		# so we must encode it into JPEG in order to correctly display the
-		# video stream
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		faces_detected = face_detection_webcam.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-		for (x, y, w, h) in faces_detected:
-			cv2.rectangle(img, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
 		resize = cv2.resize(img, (640, 480), interpolation = cv2.INTER_LINEAR) 
 		frame_flip = cv2.flip(resize,1)
 		ret, jpeg = cv2.imencode('.jpg', frame_flip)
